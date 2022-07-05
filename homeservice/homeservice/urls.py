8f2d3053e22f26734f040837c69005bdc42018ce
unknown <jirenerose@gmail.com>
2022-07-05 15:35:54 +0530

    create template for index(landing page)

--- homeservice/homeservice/urls.py
+++ homeservice/homeservice/urls.py
@@ -35,4 +35,3 @@ urlpatterns += [
 ]
 # Use static() to add URL mapping to serve static files during development (only)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
