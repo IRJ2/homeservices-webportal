32b1db105f50badcda8c03d10ebdddfe4ebdc56e
unknown <jirenerose@gmail.com>
2022-07-05 17:56:07 +0530

    create client login page

--- homeservice/catalog/urls.py
+++ homeservice/catalog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('client_login/', views.client_login, name='client_login'),
 ]
--- homeservice/catalog/views.py
+++ homeservice/catalog/views.py
@@ -10,3 +10,13 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+def client_login(request):
+    """View function for home page of site."""
+
+    context = {
+
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'client_login.html', context=context)
