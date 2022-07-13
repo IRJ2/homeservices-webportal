2fda1092cc3657319f1dea54fe035087c60acdc7
HEMA K SHAJI <hemakshaji777@gmail.com>
2022-07-13 09:49:39 -0400

    User Module

--- homeservice/catalog/models.py
+++ homeservice/catalog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    c_uid = models.CharField(max_length=20)
+    c_password = models.CharField(max_length=20)
+    c_fname = models.CharField(max_length=20)
+    c_lname = models.CharField(max_length=20)
+    
+    def __str__(self) :
+        return self.c_fname
