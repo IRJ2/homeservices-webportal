08b20ff4bb6c2f4096e3b8c8642a320155f74f46
HEMA K SHAJI <hemakshaji777@gmail.com>
2022-07-13 10:00:10 -0400

    Documented model of user

--- homeservice/catalog/models.py
+++ homeservice/catalog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 class Customer(models.Model):
-    c_uid = models.CharField(max_length=20)
-    c_password = models.CharField(max_length=20)
-    c_fname = models.CharField(max_length=20)
-    c_lname = models.CharField(max_length=20)
+    """Model of user module"""
+    c_uid = models.CharField(max_length=20) #User's Id
+    c_password = models.CharField(max_length=20) #User's password
+    c_fname = models.CharField(max_length=20) #User's first name
+    c_lname = models.CharField(max_length=20) #User's last name
     
     def __str__(self) :
-        return self.c_fname
+        """Returns the attribute in admin page"""
+        return self.c_fname 
