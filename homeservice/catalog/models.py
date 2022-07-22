32b2d91db629e16868b3a692209ea14ebcfb9280
jayageetha <jayasgeethas@gmail.com>
2022-07-22 11:36:02 +0530

    creted worker and review model

--- homeservice/catalog/models.py
+++ homeservice/catalog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class worker(models.Model):
+
+    First_Name=models.CharField(max_length=200)
+    Last_Name=models.CharField(max_length=200,blank=True)
+    W_Id=models.IntegerField(primary_key=True)
+    E_Mail=models.EmailField(max_length=300,blank=True,null=True)
+    W_Category=models.CharField(max_length=200)
+    W_Pin=models.IntegerField()
+    w_Phno=models.IntegerField()
+    Rate_P_Hour=models.IntegerField()
+
+    def __str__(self):
+        return self.W_Id
+
+class review(models.Model):
+
+    W_Id=models.ForeignKey('worker',on_delete=models.SET_NULL,null=True)
+    U_id=models.ForeignKey('user',on_delete=models.SET_NULL,null=True)
+    U_review=models.TextField()
+
+    def __str__(self):
+        return self.W_Id
+
+
+    
+
+
