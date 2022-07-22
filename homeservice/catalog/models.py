34d68cec08e2fbc845531695b09fca1580f1582b
unknown <jirenerose@gmail.com>
2022-07-22 08:22:35 +0530

    add update

--- homeservice/catalog/models.py
+++ homeservice/catalog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    """Model of user module"""
+    c_uid = models.CharField(max_length=20) #User's Id
+    c_password = models.CharField(max_length=20) #User's password
+    c_fname = models.CharField(max_length=20) #User's first name
+    c_lname = models.CharField(max_length=20) #User's last name
+    
+    def __str__(self) :
+        """Returns the attribute in admin page"""
+        return self.c_fname 
