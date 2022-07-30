e2bec6b3a802ff8fe4a48b1d5772d2293388de35
unknown <jirenerose@gmail.com>
2022-07-30 18:16:44 +0530

    update cache

--- homeservice/catalog/urls.py
+++ homeservice/catalog/urls.py
@@ -12,10 +12,6 @@ urlpatterns = [
     path('expert_profile/', views.expertProfile, name='expert_profile'),
     path('expert_signup/', views.expertSignup, name='expert_signup'),
     path('expert_dis_profile/', views.expertDisplayProfile, name='expert_display_profile'),
-<<<<<<< HEAD
-
-=======
->>>>>>> 4a28cc583c87becd2b8389bc33aee0045ee13550
     path('plumbing_service/', views.PlumbingListView.as_view(), name='plumbing_list'),
     path('ac_service/', views.ACServicingListView.as_view(), name='ac_list'),
     path('carpentry_service/', views.CarpentryListView.as_view(), name='carpentry_list'),
@@ -23,11 +19,5 @@ urlpatterns = [
     path('homecleaning_service/', views.HomeCleaningListView.as_view(), name='homecleaning_list'),
     path('laptoprepair_service/', views.LaptopRepairListView.as_view(), name='laptoprepair_list'),
     path('plumbing_service/<str:pk>', views.ExpertDetailView.as_view(), name='expert_detail'),
-<<<<<<< HEAD
-
     path('logout/', views.logout, name='logout'),
-=======
-    path('logout/', views.logout, name='logout'),
-
->>>>>>> 4a28cc583c87becd2b8389bc33aee0045ee13550
 ]
